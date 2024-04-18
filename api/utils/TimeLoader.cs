3a66af2bd7857b2a30b1ebad2ae95093d39d865d
alexchoicy <alex123321choicy@gmail.com>
2024-04-18 20:59:14 +0800

    fix: timeLoader incorrect path referece

--- api/utils/TimeLoader.cs
+++ api/utils/TimeLoader.cs
@@ -8,16 +8,24 @@ namespace api.utils
 {
     public class TimeLoader
     {
-        public static string folderPath = "../Config/TimeConfig.json";
+        public static string GetTimeConfigPath()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var timeConfigPath = Path.Combine(currentDirectory, "Config", "TimeConfig.json");
 
+            return timeConfigPath;
+        }
         public static DateTime GetTime()
         {
+            string folderPath = GetTimeConfigPath();
             if (!File.Exists(folderPath))
             {
+                Console.WriteLine("Time config file not found");
                 return DateTime.Now;
             }
 
             string json = File.ReadAllText(folderPath);
+            Console.WriteLine("Time config: " + json);
             TimeConfig timeConfig = JsonConvert.DeserializeObject<TimeConfig>(json);
             if (timeConfig.useCustomTime)
             {
